55a8394a68c508d50c0f9d517f2c5ffd8dcc397b
Yvette <nchombuayvta@gmil.com>
2022-02-28 09:51:08 +0100

    vue intro

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = vue.createApp({
+    data(){
+        return {
+            title: 'the final empire',
+            author: 'Tanila yvete',
+            age:45
+        }
+    }
+})
+app.mount('#app')
