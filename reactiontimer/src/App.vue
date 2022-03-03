f0526ca98499aeb6b96ef0a62f756d146d694461
Yvette <nchombuayvta@gmil.com>
2022-03-03 19:01:53 +0100

    reaction timer
    reaction time

---
+++ reactiontimer/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+ <h1>Ninja Reacton Time</h1>
+ <button @click="start" :disabled="isPlaying">play</button>
+ <Block v-if="isPlaying" :delay='delay'/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+
+export default {
+  name: "App",
+  components: {
+    Block
+  },
+  data(){
+    return{
+      isPlaying:false,
+      delay:null
+
+    }
+
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random()* 5000
+      this.isPlaying = true
+      console.log(this.delay)
+
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ reactiontimer/src/components/Block.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+      click me 
+  </div>
+</template>
+
+<script>
+import { onUnmounted, onUpdated } from '@vue/runtime-core'
+export default {
+    props:['delay'],
+    data(){
+        return{
+            showBlock:false,
+            timer:null,
+            reactionTime:0
+        }
+    },
+    mounted(){
+        console.log('component mounted')
+        setTimeout(()=>{
+            this.showBlock=true
+        },this.delay)
+    },
+    methods:{
+        startTimer(){
+            this.timer= setInterval(()=>{
+                this.reactionTime+=10
+                
+            },10)
+        },
+        stopTimer(){
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+        }
+    }
+
+  
+
+}
+</script>
+
+<style>
+.block{
+    width:400px;
+    border-radius:20px;
+    background: #0faf87;
+    color: #fff;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+
+}
+
+</style>
